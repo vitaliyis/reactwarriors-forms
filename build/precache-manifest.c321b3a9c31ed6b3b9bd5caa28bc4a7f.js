6c7b0e8863f8f0f79ac464af4d5611807dc46273
vitaliyis <vetal.friend@gmail.com>
2020-03-21 14:37:50 +0200

    deploy to github pages1

---
+++ build/precache-manifest.c321b3a9c31ed6b3b9bd5caa28bc4a7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d87ca4e07b6c375f6e7",
+    "url": "/static/css/main.b86cc451.chunk.css"
+  },
+  {
+    "revision": "8d87ca4e07b6c375f6e7",
+    "url": "/static/js/main.662a5cb0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "840a7ecaf0eec6dfa6b1",
+    "url": "/static/css/2.135c4600.chunk.css"
+  },
+  {
+    "revision": "840a7ecaf0eec6dfa6b1",
+    "url": "/static/js/2.13e278b4.chunk.js"
+  },
+  {
+    "revision": "59337baed905d6196632ae1986fb2086",
+    "url": "/static/media/shadow.59337bae.png"
+  },
+  {
+    "revision": "818d7551e939c819274cde46fb829c09",
+    "url": "/index.html"
+  }
+];
